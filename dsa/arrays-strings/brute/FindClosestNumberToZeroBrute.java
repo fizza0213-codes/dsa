04bb68fc076a503d01dd275a2e6ebf7fe020a3d0
Fizza Afzal <282662399+fizza0213-codes@users.noreply.github.com>
2026-06-25 18:48:33 +0500

    Update FindClosestNumberToZeroBrute.java
     add brute force solution for Find Closest Number To Zero

--- dsa/arrays-strings/brute/FindClosestNumberToZeroBrute.java
+++ dsa/arrays-strings/brute/FindClosestNumberToZeroBrute.java
@@ -1,4 +1,22 @@
+// Time: O(n) Space: O(1)
+// Brute force: scan all numbers, track closest to zero by absolute value
+class Solution {
+    public int findClosestNumber(int[] nums) {
+        int closest = nums[0];
+int minDist = Math.abs(nums[0]);
+for (int i = 1; i < nums.length; i++) {
+    int dist = Math.abs(nums[i]);
+    if (dist < minDist || (dist == minDist && nums[i] > closest)) {
+        minDist = dist;
+        closest = nums[i];
+    }
+}
+return closest;
+    }
+}
 
-public class FindClosestNumberToZeroBrute {
-
+public static void main(String[] args) {
+        int[] nums = {-4, -2, 1, 4, 8};
+        System.out.println(findClosest(nums)); // 1
+    }
 }

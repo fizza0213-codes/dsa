f4cee1d8b13b07456ec7623017bbfe3be373dfac
Fizza Afzal <282662399+fizza0213-codes@users.noreply.github.com>
2026-06-25 22:41:53 +0500

    Update LongestCommonPrefixBrute.java
     add brute force solution for Summary Ranges

--- dsa/arrays-strings/brute/LongestCommonPrefixBrute.java
+++ dsa/arrays-strings/brute/LongestCommonPrefixBrute.java
@@ -1,4 +1,18 @@
-
+// Time: O(n*m) Space: O(1)
 public class LongestCommonPrefixBrute {
-
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (!strs[i].startsWith(prefix)) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        }
+        return prefix;
+    }
+  public static void main(String[] args) {
+        int[] nums = {0,1,2,4,5,7};
+        System.out.println(summaryRanges(nums)); // [0->2, 4->5, 7]
+    }
 }

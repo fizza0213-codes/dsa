c2b5d476f39a1ead46b0d62e46e457fa5dd58900
Fizza Afzal <282662399+fizza0213-codes@users.noreply.github.com>
2026-06-25 22:46:23 +0500

    Update LongestCommonPrefixOptimal.java
    add optimal solution for Longest Common Prefix

--- dsa/arrays-strings/optimal/LongestCommonPrefixOptimal.java
+++ dsa/arrays-strings/optimal/LongestCommonPrefixOptimal.java
@@ -1,3 +1,15 @@
-
+// Time: O(n log n) Space: O(1)
 public class LongestCommonPrefixOptimal {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) return "";
+        Arrays.sort(strs);
+        String first = strs[0], last = strs[strs.length - 1];
+        int i = 0;
+        while (i < first.length() && i < last.length() && first.charAt(i) == last.charAt(i)) i++;
+        return first.substring(0, i);
+    }
+  public static void main(String[] args) {
+        String[] strs = {"flower","flow","flight"};
+        System.out.println(longestCommonPrefix(strs)); // fl
+    }
 }

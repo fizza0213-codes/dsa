09c794fafe0760066a2fe84806111b88ad45e499
Fizza Afzal <282662399+fizza0213-codes@users.noreply.github.com>
2026-06-25 22:19:05 +0500

    Update IsSubsequenceOptimal.java
     add optimal solution for Is Subsequence

--- dsa/arrays-strings/optimal/IsSubsequenceOptimal.java
+++ dsa/arrays-strings/optimal/IsSubsequenceOptimal.java
@@ -1,3 +1,17 @@
-
+// Time: O(n+m) Space: O(1)
 public class IsSubsequenceOptimal {
+    public boolean isSubsequence(String s, String t) {
+        int i = 0, j = 0;
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) i++;
+            j++;
+        }
+        return i == s.length();
+    }
 }
+
+    public static void main(String[] args) {
+        System.out.println(isSubsequence("abc", "ahbgdc")); // true
+    }
+}
+

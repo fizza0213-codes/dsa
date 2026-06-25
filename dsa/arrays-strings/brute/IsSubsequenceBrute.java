9c93618bc1127b3a5c8a0dc1e95278bb4a5d658b
Fizza Afzal <282662399+fizza0213-codes@users.noreply.github.com>
2026-06-25 22:15:04 +0500

    Update IsSubsequenceBrute.java
     add brute force solution for Is Subsequence

--- dsa/arrays-strings/brute/IsSubsequenceBrute.java
+++ dsa/arrays-strings/brute/IsSubsequenceBrute.java
@@ -1,4 +1,24 @@
-
+// Time: O(n*m) Space: O(1)
 public class IsSubsequenceBrute {
-
+    public boolean isSubsequence(String s, String t) {
+        int sLen = s.length(), tLen = t.length();
+        int tStart = 0;
+        for (int i = 0; i < sLen; i++) {
+            boolean found = false;
+            for (int j = tStart; j < tLen; j++) {
+                if (t.charAt(j) == s.charAt(i)) {
+                    found = true;
+                    tStart = j + 1;
+                    break;
+                }
+            }
+            if (!found) return false;
+        }
+        return true;
+    }
 }
+public static void main(String[] args) {
+        System.out.println(isSubsequence("abc", "ahbgdc")); // true
+    }
+}
+

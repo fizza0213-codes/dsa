810454b4d554dbc7d7793498ce60aa17b67c479d
Fizza Afzal <282662399+fizza0213-codes@users.noreply.github.com>
2026-06-25 22:56:14 +0500

    Update BestTimeToBuySellStockOptimal.java
     add optimal solution for Best Time To Buy And Sell Stock

--- dsa/arrays-strings/optimal/BestTimeToBuySellStockOptimal.java
+++ dsa/arrays-strings/optimal/BestTimeToBuySellStockOptimal.java
@@ -1,4 +1,16 @@
 
+// Time: O(n) Space: O(1)
 public class BestTimeToBuySellStockOptimal {
-
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE, maxProfit = 0;
+        for (int price : prices) {
+            if (price < minPrice) minPrice = price;
+            else if (price - minPrice > maxProfit) maxProfit = price - minPrice;
+        }
+        return maxProfit;
+    }
+  public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+        System.out.println(maxProfit(prices)); // 5
+    }
 }

6d17dbe113f3227c1c37668cd5c87a76f3bd2013
Fizza Afzal <282662399+fizza0213-codes@users.noreply.github.com>
2026-06-25 22:52:23 +0500

    Update BestTimeToBuySellStockBrute.java
    add brute force solution for Best Time To Buy And Sell Stock

--- dsa/arrays-strings/brute/BestTimeToBuySellStockBrute.java
+++ dsa/arrays-strings/brute/BestTimeToBuySellStockBrute.java
@@ -1,3 +1,17 @@
-
+// Time: O(n^2) Space: O(1)
 public class BestTimeToBuySellStockBrute {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                int profit = prices[j] - prices[i];
+                if (profit > maxProfit) maxProfit = profit;
+            }
+        }
+        return maxProfit;
+    }
+   public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+        System.out.println(maxProfit(prices)); // 5
+    }
 }

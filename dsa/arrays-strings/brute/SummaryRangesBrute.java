2fa90e725ef53be4352d0bdf6ff04af737d29ac7
Fizza Afzal <282662399+fizza0213-codes@users.noreply.github.com>
2026-06-25 22:30:15 +0500

    Update SummaryRangesBrute.java
     add brute force solution for Summary Ranges

--- dsa/arrays-strings/brute/SummaryRangesBrute.java
+++ dsa/arrays-strings/brute/SummaryRangesBrute.java
@@ -1,4 +1,35 @@
 
-public class SummaryRangesBrute {
 
+// Time: O(n^2) Space: O(n)
+public class SummaryRangesBrute {
+    public List<String> summaryRanges(int[] nums) {
+               List<String> result = new ArrayList<>();
+        int n = nums.length;
+        boolean[] used = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            if (used[i]) continue;
+            int start = nums[i], end = nums[i];
+            used[i] = true;
+            boolean extended = true;
+            while (extended) {
+                extended = false;
+                for (int j = 0; j < n; j++) {
+                    if (!used[j] && nums[j] == end + 1) {
+                        end = nums[j];
+                        used[j] = true;
+                        extended = true;
+                    }
+                }
+            }
+            result.add(start == end ? String.valueOf(start) : start + "->" + end);
+        }
+        return result;     
+    }
 }
+ public static void main(String[] args) {
+        int[] nums = {0,1,2,4,5,7};
+        System.out.println(summaryRanges(nums)); // [0->2, 4->5, 7]
+    }
+}
+
+

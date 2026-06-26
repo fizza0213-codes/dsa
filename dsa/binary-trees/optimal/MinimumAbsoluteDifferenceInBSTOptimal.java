2d163725d9e5ee3c56ded83981dc07236ac14309
Fizza Afzal <282662399+fizza0213-codes@users.noreply.github.com>
2026-06-26 10:39:18 +0500

    Update MinimumAbsoluteDifferenceInBSTOptimal.java
    add optimal solution for   Minimum Absolute Difference In BST

--- dsa/binary-trees/optimal/MinimumAbsoluteDifferenceInBSTOptimal.java
+++ dsa/binary-trees/optimal/MinimumAbsoluteDifferenceInBSTOptimal.java
@@ -1,1 +1,26 @@
 
+// Time: O(n^2) Space: O(n)
+public class MinimumAbsoluteDifferenceInBSTOptimal {
+    private Integer prev = null;
+    private int minDiff = Integer.MAX_VALUE;
+    public int getMinimumDifference(TreeNode root) {
+        inorder(root);
+        return minDiff;
+    }
+    private void inorder(TreeNode node) {
+        if (node == null) return;
+        inorder(node.left);
+        if (prev != null) minDiff = Math.min(minDiff, node.val - prev);
+        prev = node.val;
+        inorder(node.right);
+    }
+  public static void main(String[] args) {
+        TreeNode root = new TreeNode(4);
+        root.startIndex = new TreeNode(2);
+        root.endIndex = new TreeNode(6);
+        root.startIndex.startIndex = new TreeNode(1);
+        root.startIndex.endIndex = new TreeNode(3);
+        System.out.println(getMinimumDifference(root)); // 1
+    }
+  
+}

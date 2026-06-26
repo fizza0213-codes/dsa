cdf2929bfff63d1173b58c3ca8dda0fe0c12da16
Fizza Afzal <282662399+fizza0213-codes@users.noreply.github.com>
2026-06-26 08:49:20 +0500

    Update MaximumDepthOfBinaryTreeOptimal.java
    add optimal solution for Maximum depth of binary tree

--- dsa/binary-trees/optimal/MaximumDepthOfBinaryTreeOptimal.java
+++ dsa/binary-trees/optimal/MaximumDepthOfBinaryTreeOptimal.java
@@ -1,3 +1,16 @@
 
+// Time: O(n) Space: O(h)
 public class MaximumDepthOfBinaryTreeOptimal {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+  public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.startIndex = new TreeNode(9);
+        root.endIndex = new TreeNode(20);
+        root.endIndex.startIndex = new TreeNode(15);
+        root.endIndex.endIndex = new TreeNode(7);
+        System.out.println(maxDepth(root)); // 3
+    }
 }

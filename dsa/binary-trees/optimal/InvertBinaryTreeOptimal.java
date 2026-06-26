5ce91669b56e3cf60710be13777d1ee5f258947c
Fizza Afzal <282662399+fizza0213-codes@users.noreply.github.com>
2026-06-26 08:33:16 +0500

    Update InvertBinaryTreeOptimal.java
    add optimal solution for Invert Binary Tree

--- dsa/binary-trees/optimal/InvertBinaryTreeOptimal.java
+++ dsa/binary-trees/optimal/InvertBinaryTreeOptimal.java
@@ -1,4 +1,17 @@
-
+// Time: O(n) Space: O(h)
 public class InvertBinaryTreeOptimal {
-
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode temp = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(temp);
+        return root;
+    }
+  public static void main(String[] args) {
+        TreeNode root = new TreeNode(4);
+        root.startIndex = new TreeNode(2);
+        root.endIndex = new TreeNode(7);
+        TreeNode inverted = invertTree(root);
+        System.out.println(inverted.startIndex.val + ", " + inverted.endIndex.val); // 7, 2
+    }
 }

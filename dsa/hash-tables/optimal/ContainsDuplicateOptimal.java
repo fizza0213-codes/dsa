e6fdc9bb278bc61376fa4f42acf957b99b697f3b
Fizza Afzal <282662399+fizza0213-codes@users.noreply.github.com>
2026-06-27 16:20:50 +0500

    Add optimal containsDuplicate method
    Implement containsDuplicate method using HashSet to check for duplicates in an array.

--- dsa/hash-tables/optimal/ContainsDuplicateOptimal.java
+++ dsa/hash-tables/optimal/ContainsDuplicateOptimal.java
@@ -1,4 +1,16 @@
 
+// Time: O(n) Space: O(n)
 public class ContainsDuplicateOptimal {
+   public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (!seen.add(num)) return true;
+        }
+        return false;
+    }
+  public static void main(String[] args) {
+        int[] nums = {1,2,3,1};
+        System.out.println(containsDuplicate(nums)); // true
+    }
 
 }

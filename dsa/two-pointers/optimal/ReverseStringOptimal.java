3b683b48d2ed3aeb27b5c05ca738631e123601f5
Fizza Afzal <282662399+fizza0213-codes@users.noreply.github.com>
2026-06-27 19:19:22 +0500

    Add optimal reverse string implementation
    Implemented an optimal reverse string method using two pointers.

--- dsa/two-pointers/optimal/ReverseStringOptimal.java
+++ dsa/two-pointers/optimal/ReverseStringOptimal.java
@@ -1,4 +1,19 @@
 
+// Time: O(n) Space: O(1)
 public class ReverseStringOptimal {
+  public void reverseString(char[] s) {
+        int left = 0, right = s.length - 1;
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++; right--;
+        }
+    }
+  public static void main(String[] args) {
+        char[] s = {'h','e','l','l','o'};
+        reverseString(s);
+        System.out.println(new String(s)); // olleh
+    }
 
 }

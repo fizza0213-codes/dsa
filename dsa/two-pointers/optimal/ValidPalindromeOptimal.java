578d8093d37ba2669b5e3401616dfe27f91b1eb3
Fizza Afzal <282662399+fizza0213-codes@users.noreply.github.com>
2026-06-27 19:50:27 +0500

    Add optimal method palindrome checking method in ValidPalindromeOptimal
    Implement isPalindrome method to check if a string is a palindrome, ignoring non-alphanumeric characters and case.

--- dsa/two-pointers/optimal/ValidPalindromeOptimal.java
+++ dsa/two-pointers/optimal/ValidPalindromeOptimal.java
@@ -1,4 +1,17 @@
-
+// Time: O(n) Space: O(1)
 public class ValidPalindromeOptimal {
 
+   public boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) left++;
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) right--;
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) return false;
+            left++; right--;
+        }
+        return true;
+    }
+  public static void main(String[] args) {
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama")); // true
+    }
 }

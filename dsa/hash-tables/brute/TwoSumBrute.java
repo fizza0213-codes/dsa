10d589533a86cce8cfd8b2e724f9051cc6572df4
Fizza Afzal <282662399+fizza0213-codes@users.noreply.github.com>
2026-06-27 16:24:49 +0500

    Add brute force solution for Two Sum problem
    This implementation uses a nested loop to find two indices in the array that sum up to the target value, returning their indices or [-1, -1] if no such pair exists. The main method demonstrates the functionality with a sample input.

--- dsa/hash-tables/brute/TwoSumBrute.java
+++ dsa/hash-tables/brute/TwoSumBrute.java
@@ -1,4 +1,19 @@
 
+// Time: O(n) Space: O(n)
 public class TwoSumBrute {
+public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) return new int[]{i, j};
+            }
+        }
+        return new int[]{-1, -1};
+    }
 
+  public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int[] outputValue = twoSum(nums, 9);
+        System.out.println(outputValue[0] + ", " + outputValue[1]); // 0, 1
+    }
+  
 }

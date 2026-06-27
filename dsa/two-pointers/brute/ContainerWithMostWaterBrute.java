87b58a1c089492010d6756d732c1b544e2a11cc3
Fizza Afzal <282662399+fizza0213-codes@users.noreply.github.com>
2026-06-27 20:09:10 +0500

    Add brute-force method to calculate max water area
    Implemented brute-force solution to find maximum area.

--- dsa/two-pointers/brute/ContainerWithMostWaterBrute.java
+++ dsa/two-pointers/brute/ContainerWithMostWaterBrute.java
@@ -1,4 +1,18 @@
 
+// Time: O(n^2) Space: O(1)
 public class ContainerWithMostWaterBrute {
-
+  public int maxArea(int[] height) {
+        int maxArea = 0;
+        for (int i = 0; i < height.length; i++) {
+            for (int j = i + 1; j < height.length; j++) {
+                int area = Math.min(height[i], height[j]) * (j - i);
+                maxArea = Math.max(maxArea, area);
+            }
+        }
+        return maxArea;
+    }
+  public static void main(String[] args) {
+        int[] height = {1,8,6,2,5,4,8,3,7};
+        System.out.println(maxArea(height)); // 49
+    }
 }

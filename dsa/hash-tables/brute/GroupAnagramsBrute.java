a29cce784e1e3829ba88a86d0cb2c5e071c3ca2a
Fizza Afzal <282662399+fizza0213-codes@users.noreply.github.com>
2026-06-27 16:51:50 +0500

    Implement brute force approach for anagram grouping

--- dsa/hash-tables/brute/GroupAnagramsBrute.java
+++ dsa/hash-tables/brute/GroupAnagramsBrute.java
@@ -1,4 +1,28 @@
 
 public class GroupAnagramsBrute {
+   public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<>();
+        boolean[] used = new boolean[strs.length];
+        for (int i = 0; i < strs.length; i++) {
+            if (used[i]) continue;
+            List<String> group = new ArrayList<>();
+            group.add(strs[i]);
+            used[i] = true;
+            for (int j = i + 1; j < strs.length; j++) {
+                if (!used[j] && isAnagram(strs[i], strs[j])) {
+                    group.add(strs[j]);
+                    used[j] = true;
+                }
+            }
+            result.add(group);
+        }
+        return result;
+    }
+    private boolean isAnagram(String a, String b) {
+        char[] ca = a.toCharArray(), cb = b.toCharArray();
+        Arrays.sort(ca);
+        Arrays.sort(cb);
+        return Arrays.equals(ca, cb);
+    }
 
 }

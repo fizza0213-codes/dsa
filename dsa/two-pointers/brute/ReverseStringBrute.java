7d46cb662cf30e30dcdb9607cc4b3134b0855116
Fizza Afzal <282662399+fizza0213-codes@users.noreply.github.com>
2026-06-27 19:16:17 +0500

    Add brute-force string reversal method
    Implemented a brute-force method to reverse a character array.

--- dsa/two-pointers/brute/ReverseStringBrute.java
+++ dsa/two-pointers/brute/ReverseStringBrute.java
@@ -1,4 +1,14 @@
-
+// Time: O(n) Space: O(n)
 public class ReverseStringBrute {
-
+public void reverseString(char[] s) {
+        char[] temp = new char[s.length];
+        for (int i = 0; i < s.length; i++) temp[i] = s[s.length - 1 - i];
+        for (int i = 0; i < s.length; i++) s[i] = temp[i];
+    }
+public static void main(String[] args) {
+        char[] s = {'h','e','l','l','o'};
+        reverseString(s);
+        System.out.println(new String(s)); // olleh
+    }
+  
 }

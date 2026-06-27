f691365901f96fd0e0950ed7211cf32b9e722052
Fizza Afzal <282662399+fizza0213-codes@users.noreply.github.com>
2026-06-27 16:57:19 +0500

    Add groupAnagrams method to GroupAnagramsOptimal class
    Implemented a optimal method to group anagrams from an array of strings using a hash map.

--- dsa/hash-tables/optimal/GroupAnagramsOptimal.java
+++ dsa/hash-tables/optimal/GroupAnagramsOptimal.java
@@ -1,4 +1,18 @@
-
+// Time: O(n * k log k) Space: O(n*k)
 public class GroupAnagramsOptimal {
+  public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+  public static void main(String[] args) {
+        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+        System.out.println(groupAnagrams(strs));
+    }
 
 }

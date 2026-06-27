d5079a64daafde868b5efb3765330b449114008e
Fizza Afzal <282662399+fizza0213-codes@users.noreply.github.com>
2026-06-27 15:23:20 +0500

    Implement lastStoneWeight method in LastStoneWeightBrute
    add brute solution for  LastStoneWeightBrute

--- dsa/heaps/brute/LastStoneWeightBrute.java
+++ dsa/heaps/brute/LastStoneWeightBrute.java
@@ -1,4 +1,19 @@
 
+// Time: O(n^2 log n) Space: O(n)
 public class LastStoneWeightBrute {
-
+public int lastStoneWeight(int[] stones) {
+        List<Integer> list = new ArrayList<>();
+        for (int s : stones) list.add(s);
+        while (list.size() > 1) {
+            Collections.sort(list, Collections.reverseOrder());
+            int y = list.remove(0);
+            int x = list.remove(0);
+            if (y != x) list.add(0, y - x);
+        }
+        return list.isEmpty() ? 0 : list.get(0);
+    }
+  public static void main(String[] args) {
+        int[] stones = {2,7,4,1,8,1};
+        System.out.println(lastStoneWeight(stones)); // 1
+    }
 }

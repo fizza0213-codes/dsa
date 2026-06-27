85fe79e4de236d26e7cd1e5ed97a789ad34943c9
Fizza Afzal <282662399+fizza0213-codes@users.noreply.github.com>
2026-06-27 20:12:40 +0500

    Add optimal solution for Container With Most Water
    Implemented an optimal solution for finding the maximum area of water that can be contained using two pointers.

--- dsa/two-pointers/optimal/ContainerWithMostWaterOptimal.java
+++ dsa/two-pointers/optimal/ContainerWithMostWaterOptimal.java
@@ -1,4 +1,18 @@
 
+// Time: O(n) Space: O(1)
 public class ContainerWithMostWaterOptimal {
+  public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1, maxArea = 0;
+        while (left < right) {
+            int area = Math.min(height[left], height[right]) * (right - left);
+            maxArea = Math.max(maxArea, area);
+            if (height[left] < height[right]) left++; else right--;
+        }
+        return maxArea;
+    }
+  public static void main(String[] args) {
+        int[] height = {1,8,6,2,5,4,8,3,7};
+        System.out.println(maxArea(height)); // 49
+    }
 
 }

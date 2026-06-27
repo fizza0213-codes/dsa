36fdcefa1ad5574c4ca42a4623275759a2412a8d
Fizza Afzal <282662399+fizza0213-codes@users.noreply.github.com>
2026-06-27 16:40:09 +0500

    Add method to calculate maximum number of balloons
    Implement maxNumberOfBalloons method to count maximum balloons from given text.

--- dsa/hash-tables/optimal/MaximumNumberOfBalloonsOptimal.java
+++ dsa/hash-tables/optimal/MaximumNumberOfBalloonsOptimal.java
@@ -1,4 +1,17 @@
 
+// Time: O(n) Space: O(1)
 public class MaximumNumberOfBalloonsOptimal {
-
+public int maxNumberOfBalloons(String text) {
+        int[] freq = new int[26];
+        for (char c : text.toCharArray()) freq[c - 'a']++;
+        int b = freq['b' - 'a'];
+        int a = freq['a' - 'a'];
+        int l = freq['l' - 'a'] / 2;
+        int o = freq['o' - 'a'] / 2;
+        int n = freq['n' - 'a'];
+        return Math.min(b, Math.min(a, Math.min(l, Math.min(o, n))));
+    }
+  public static void main(String[] args) {
+        System.out.println(maxNumberOfBalloons("nlaebolko")); // 1
+    }
 }

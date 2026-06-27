5bb8a58896bf342c153a21d7a78fa8206fb1ecf2
Fizza Afzal <282662399+fizza0213-codes@users.noreply.github.com>
2026-06-27 15:33:53 +0500

    Add Kth largest element finder using min-heap
    Implemented a method to find the Kth largest element using a min-heap in brute.

--- dsa/heaps/optimal/KthLargestElementOptimal.java
+++ dsa/heaps/optimal/KthLargestElementOptimal.java
@@ -1,4 +1,17 @@
 
+// Time: O(n log k) Space: O(k)
 public class KthLargestElementOptimal {
+  public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int num : nums) {
+            minHeap.offer(num);
+            if (minHeap.size() > k) minHeap.poll();
+        }
+        return minHeap.peek();
+    }
+  public static void main(String[] args) {
+        int[] nums = {3,2,1,5,6,4};
+        System.out.println(new KthLargestElementOptimal().findKthLargest(nums, 2)); // 5
+    }
 
 }
